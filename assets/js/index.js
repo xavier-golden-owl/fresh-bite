5c99e9b657751f015cd965da1a99816d7c22f4eb
xavier-golden-owl <xavier.cao.goldenowl@gmail.com>
2023-08-03 15:57:23 +0700

    process scroll bar + show more in menu function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
+  $(window).on('scroll', function() {
+    // Get the current scroll position
+    let scrollPosition = $(this).scrollTop();
 
-})
+		let percent = scrollPosition * 100 / ($(document).height() - $(window).height());
+
+		$('#scroll').css('width', percent + '%')
+  });
+
+	$('button').on({
+		click: function() {
+			if ($(this).text() === 'More Menu') {
+				let menu = $('#our-menu .second-part')
+				if (menu.children().length * 2 <= 32) {
+					const element = menu.html();
+					menu.append($.parseHTML(element))
+				}
+			}
+		}
+	})
+});
